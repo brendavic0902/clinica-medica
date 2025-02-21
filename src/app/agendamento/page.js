600f95b603522be40a6d5a2947f679cb0b1e9d93
brendavic0902 <brendavitoriagomesdematos7@gmail.com>
2025-02-21 10:02:12 -0400

    socorro

--- src/app/agendamento/page.js
+++ src/app/agendamento/page.js
@@ -10,7 +10,7 @@ function Agendamento() {
   const [tipo, setTipo] = useState('');
   const [mensagem, setMensagem] = useState('');
 
-  const pacientes = ['Paciente 1', 'Paciente 3', 'Paciente 4'];
+  const pacientes = ['Paciente 1', 'Paciente 2', 'Paciente 3'];
   const medicos = ['Médico 1', 'Médico 2', 'Médico 3'];
   const tipos = ['Consulta', 'Exame', 'Retorno'];
   const horas = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
